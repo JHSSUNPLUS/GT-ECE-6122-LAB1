4915efa4b40c9489bbbaf9bc80c67fd4e227c267
Jiahao Sun <sjh29074811@163.com>
2022-09-16 14:31:54 +0800

    Initial Commit

---
+++ 6122_LAB_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  6122_LAB_1
+//
+//  Created by Jiahao Sun on 2022/9/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
